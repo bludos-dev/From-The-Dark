8efb4c57e7eecc47c22a98f4238b2ebe90769943
bludos_dev <bluosdev69@gmail.com>
2025-02-03 03:28:38 +0530

    Update MainMod.java

--- src/main/java/com/blud/thedark/MainMod.java
+++ src/main/java/com/blud/thedark/MainMod.java
@@ -2,21 +2,14 @@ package com.blud.thedark;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -25,58 +18,50 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
-@Mod(TheDark.MODID)
-public class TheDark {
+@Mod(MainMod.MODID)
+public class MainMod {
     public static final String MODID = "thedark";
     private static final Logger LOGGER = LogUtils.getLogger();
-
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    
+    // Register items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    public static final RegistryObject<Block> DARK_BLOCK = BLOCKS.register("dark_block", () ->
-            new Block(BlockBehaviour.Properties.of().mapColor(MapColor.BLACK)));
-
-    public static final RegistryObject<Item> DARK_BLOCK_ITEM = ITEMS.register("dark_block", () ->
-            new BlockItem(DARK_BLOCK.get(), new Item.Properties()));
-
-    public static final RegistryObject<Item> DARK_ITEM = ITEMS.register("dark_item", () ->
-            new Item(new Item.Properties().food(new FoodProperties.Builder()
-                    .alwaysEat().nutrition(2).saturationMod(1.5f).build())));
-
-    public static final RegistryObject<CreativeModeTab> DARK_TAB = CREATIVE_MODE_TABS.register("dark_tab", () ->
-            CreativeModeTab.builder()
-                    .withTabsBefore(CreativeModeTabs.COMBAT)
-                    .icon(() -> DARK_ITEM.get().getDefaultInstance())
-                    .displayItems((parameters, output) -> output.accept(DARK_ITEM.get()))
-                    .build());
-
-    public TheDark(FMLJavaModLoadingContext context) {
+    public MainMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
+        // Register common setup
         modEventBus.addListener(this::commonSetup);
-        BLOCKS.register(modEventBus);
+        
+        // Register items
         ITEMS.register(modEventBus);
-        CREATIVE_MODE_TABS.register(modEventBus);
 
+        // Register Forge events
         MinecraftForge.EVENT_BUS.register(this);
+
+        // Register config
+        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("Initializing The Dark mod...");
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.logDirtBlock)
+            LOGGER.info("DIRT BLOCK LOGGING ENABLED");
+
+        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
     }
 
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        LOGGER.info("The Dark mod server is starting...");
+        LOGGER.info("HELLO from server starting");
     }
 
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            LOGGER.info("Client setup for The Dark mod...");
-            LOGGER.info("Player: {}", Minecraft.getInstance().getUser().getName());
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
 }

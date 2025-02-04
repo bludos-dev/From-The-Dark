09b39e032ad9889f17c01ec40408ee8f2633ab1e
bludos_dev <bluosdev69@gmail.com>
2025-02-04 11:26:29 +0530

    Update MainMod.java

--- src/main/java/com/blud/thedark/MainMod.java
+++ src/main/java/com/blud/thedark/MainMod.java
@@ -15,37 +15,32 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+import software.bernie.geckolib.GeckoLib;
 
 @Mod(MainMod.MODID)
 public class MainMod {
     public static final String MODID = "thedark";
     private static final Logger LOGGER = LogUtils.getLogger();
     
-    // Register items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
     public MainMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
-        // Register common setup
+        GeckoLib.initialize();
+
         modEventBus.addListener(this::commonSetup);
         
-        // Register items
         ITEMS.register(modEventBus);
 
-        // Register Forge events
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register config
         context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("HELLO FROM COMMON SETUP");
-
-        
         LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
     }
 
